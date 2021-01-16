3ca5fa796dfe6a4560a2a4bbc6066f2f8e1bf3b1
Varun Dev Sharma <dudevarun7777@gmail.com>
2021-01-16 19:15:51 +0530

    mongo error fixed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 //Connecting MongoDB
 const mongoose = require("mongoose");
 // mongodb://localhost:27017/divyarachna
-mongoose.connect("mongodb+srv:icdam:icdam_2.0@icdamv.hiyrd.mongodb.net/divyarachna", {
+mongoose.connect("mongodb+srv://icdam:icdam_2.0@icdamv.hiyrd.mongodb.net/divyarachna", {
   useNewUrlParser: !0,
   useUnifiedTopology: !0,
   useCreateIndex: 1,
